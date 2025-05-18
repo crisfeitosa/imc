e8063855aa7ecf1d49d6666b7b04fda58dccbf4d
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-18 17:06:06 -0300

    Modularizando o Modal popup

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
+import { Modal } from './modal.js';
+
 // variables
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
-
-const Modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: document.querySelector('.modal button.close'),
-  open() {
-    Modal.wrapper.classList.add('open');
-  },
-  close() {
-    Modal.wrapper.classList.remove('open');
-  }
-}
 
 form.onsubmit = event => {
   event.preventDefault();
@@ -26,10 +16,6 @@ form.onsubmit = event => {
 
   Modal.message.innerHTML = message;
   Modal.open();
-}
-
-Modal.buttonClose.onclick = () => {
-  Modal.close();
 }
 
 function IMC(weight, height) {
